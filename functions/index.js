effb51fabc999756102e880bfc95a746840e0c49
soongil kim <alasheep@gmail.com>
2019-05-16 23:30:14 +0900

    implement firebase functions (storeLocation, getLocation)

--- functions/index.js
+++ functions/index.js
@@ -24,6 +24,21 @@ const cors = require('cors')({origin: true});
 exports.saveToken = functions.https.onRequest((req, res) => {
     cors(req, res, () => {
 
+        var fcm_device_token = req.query.fcm_device_token;
+
+        // 디비 'cur_location' 컬렉션 'lLxQJ1cOQv5nU3HNNSSm' 문서의 'cur_time' 필드의 값을 요청값 body의 cur_time 값으로 업데이트 해줍니다.
+        db.collection('push_token').doc('yAmpQQ271chZWM82LtI2').update({ token: fcm_device_token/*req.body.text*/ });
+
+        res.status(200).json({
+            message: 'It worked saveTokenTest function!'+fcm_device_token
+        })
+   
+    });
+});
+
+exports.saveTokenTest = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+
         var fcm_device_token = "dtoFOvFrsNE:APA91bGfjCrdAdiJf4dQRi5HarR7fehAmVSIN8d88cAe0K2GQoDuUPtl6EDVM5CDwSW1S_JrFd3AaEYMY3bjKGD2D60nt5Dpbm4zsOddgppYyj1NGlm6xwl4RBjTpycGMp5-2i-FXku5";
 
         // 디비 'cur_location' 컬렉션 'lLxQJ1cOQv5nU3HNNSSm' 문서의 'cur_time' 필드의 값을 요청값 body의 cur_time 값으로 업데이트 해줍니다.
@@ -35,7 +50,6 @@ exports.saveToken = functions.https.onRequest((req, res) => {
    
     });
 });
-
 
 exports.sendFCM = functions.https.onRequest((req, res) => {
     cors(req, res, () => {
@@ -54,7 +68,6 @@ exports.sendFCM = functions.https.onRequest((req, res) => {
    
     });
 });
-
 
 exports.reqInfo = functions.https.onRequest((req, res) => {
     cors(req, res, () => {
@@ -88,5 +101,49 @@ exports.reqInfo = functions.https.onRequest((req, res) => {
             message: 'It worked!',
         })
     
+    });
+});
+
+exports.storeLocation = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+
+        var latitude = req.query.latitude;
+        var longtitude = req.query.longtitude;
+
+        // 디비 'cur_location' 컬렉션 'lLxQJ1cOQv5nU3HNNSSm' 문서의 'cur_time' 필드의 값을 요청값 body의 cur_time 값으로 업데이트 해줍니다.
+        db.collection('cur_location').doc('lLxQJ1cOQv5nU3HNNSSm').update({ 
+            cur_latitude: latitude,
+            cur_longtitude: longtitude 
+        });
+
+        res.status(200).json({
+            message: 'It worked storeLocation function!',
+        })
+   
+    });
+});
+
+exports.getLocation = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+
+        var latitude = req.query.latitude;
+        var longtitude = req.query.longtitude;
+
+        // 디비 'cur_location' 컬렉션 'lLxQJ1cOQv5nU3HNNSSm' 문서의 'cur_time' 필드의 값을 요청값 body의 cur_time 값으로 업데이트 해줍니다.
+        db.collection('cur_location').doc('lLxQJ1cOQv5nU3HNNSSm').get().then(doc => {
+            // 성공하면 문서안의 'text'필드 값을 응답해 줍니다.
+            return res.status(200).send({ reuslt : doc.data() });
+        }).catch(err => {
+            console.log('Error', err);
+
+            res.status(200).json({
+                message: 'It worked storeLocation function!, but happened error' + err,
+            })
+        });
+
+        // res.status(200).json({
+        //     message: 'It worked storeLocation function!',
+        // })
+   
     });
 });
